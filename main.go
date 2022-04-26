bde5d5b70db57420a103e7b1c664dc1809089b0c
ae-lexs <brian.alexis.nava.arreola@gmail.com>
2022-04-25 22:50:32 -0500

    feat: Refactor main using config and client packages.

--- main.go
+++ main.go
@@ -1,36 +1,24 @@
 package main
 
 import (
-	"github.com/ae-lexs/ae_albums_api/entity"
+	"github.com/ae-lexs/ae_albums_api/client"
+	"github.com/ae-lexs/ae_albums_api/config"
 	"github.com/ae-lexs/ae_albums_api/handler"
 	"github.com/ae-lexs/ae_albums_api/repository"
 	"github.com/ae-lexs/ae_albums_api/route"
 	"github.com/gin-gonic/gin"
-
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
 )
-
-func setUpDB() *gorm.DB {
-	postgresClient, err := gorm.Open(
-		postgres.Open(
-			"host=localhost user=album_user password=album2022 dbname=album_db port=5433 sslmode=disable",
-		),
-		&gorm.Config{},
-	)
-
-	if err != nil {
-		panic("Failed to connect to the database")
-	}
-
-	postgresClient.AutoMigrate(&entity.Album{})
-
-	return postgresClient
-}
 
 func main() {
 	router := gin.Default()
-	postgresClient := setUpDB()
+	config := config.GetConfig()
+	postgresClient := client.SetUpPostgres(
+		config.DBName,
+		config.DBHost,
+		config.DBPassword,
+		config.DBUser,
+		config.DBPort,
+	)
 	albumRepository := repository.PostgresAlbumRepository{
 		Client: postgresClient,
 	}
